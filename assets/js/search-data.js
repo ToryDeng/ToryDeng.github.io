8e452224716b440e73a1eb1853081eb9f641733c
ToryDeng <3070053397@qq.com>
2026-04-10 09:12:30 +0000

    Deploying to gh-pages from @ ToryDeng/ToryDeng.github.io@c8f4c66527aa4f59ace31499fb38299d20543cf3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -117,7 +117,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=0QI92MsAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
